f67f1bc908404607f49e040ea8796d2878eb25bd
doffylamingo <102771222+doffylamingo@users.noreply.github.com>
2024-07-27 21:13:07 +0800

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 80,
+  bracketSpacing: true,
+  "plugins": ["prettier-plugin-organize-imports"]
+};
